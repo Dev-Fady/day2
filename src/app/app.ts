9159f52277c655ad425ea05f1cb0bb6ea8d34f6d
Dev-Fady <fadymobiledev@gmail.com>
2026-02-26 18:35:20 +0200

    done day 2

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from './components/footer/footer';
+import { Header } from './components/header/header';
+import { Products } from './components/products/products';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ Header, Products, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
