99e8b8ba59e24b691c0aa2a75d5684c2e92d76a7
Dev-Fady <fadymobiledev@gmail.com>
2026-03-16 16:20:21 +0200

    Property decorator

--- src/app/components/order/order.ts
+++ src/app/components/order/order.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { Icategory } from '../../models/icategory';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
@@ -10,14 +10,15 @@ import { Products } from '../products/products';
   templateUrl: './order.html',
   styleUrl: './order.css',
 })
-export class Order {
+export class Order implements AfterViewInit {
     categories:Icategory[];
     myDate:Date = new Date();
     selectedCategory: Icategory | null = null;
 
     totalOderPrice:number = 0;
 
-
+  @ViewChild('myInput') myInput!:ElementRef  // non null assertion operator 
+  @ViewChild(Products) productsComponent!:Products;
 
     constructor() {
       this.categories = [
@@ -26,4 +27,8 @@ export class Order {
       {id:3, name:'Accessories'},
     ]
     }
+  ngAfterViewInit(): void {
+    this.myInput.nativeElement.value = "Ahmed";
+    console.log(this.productsComponent.totalOderPrice);
+  }
 }
